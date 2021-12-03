4b31907430b285f31f7ab5bf193061e57bb772f7
lekanoni20 <lekanoni18@yahoo.co.uk>
2021-12-03 18:49:23 +0000

    basic navigation added

--- index.js
+++ index.js
@@ -9,9 +9,35 @@ const app = express();
 app.use('/css',express.static(__dirname +'/css'));
 
 app.get('/', async (request, response) => { 
-
     response.send(await readFile('./home.html', 'utf8'));
-    
+});
+
+app.get('/navigation', async (request, response) => { 
+    response.send(await readFile('./navigation.html', 'utf8'));
+});
+
+app.get('/about', async (request, response) => { 
+    response.send(await readFile('./About.html', 'utf8'));
+});
+
+app.get('/importmp3Page', async (request, response) => { 
+    response.send(await readFile('./ImportMP3Page.html', 'utf8'));
+});
+
+app.get('/introduction', async (request, response) => { 
+    response.send(await readFile('./LangaugeLearningSceens/Introduction.html', 'utf8'));
+});
+
+app.get('/chinese', async (request, response) => { 
+    response.send(await readFile('./LangaugeLearningSceens/Chinese.html', 'utf8'));
+});
+
+app.get('/japanese', async (request, response) => { 
+    response.send(await readFile('./LangaugeLearningSceens/Japanese.html', 'utf8'));
+});
+
+app.get('/spanish', async (request, response) => { 
+    response.send(await readFile('./LangaugeLearningSceens/Spanish.html', 'utf8'));
 });
 
 app.listen(port, () => console.log('App avaliable on http:localhost:3000'))
